cb97e3cdfdc4e6d83550d5745da6e4150fa67f20
liuchuanren <liuchuanren@720cc348-b754-0410-aa88-cb743f661070>
2009-09-23 09:01:20 +0000

    Made changes to fix bugs in Issue 3.
    git-svn-id: https://global.googlecode.com/svn/trunk@157 720cc348-b754-0410-aa88-cb743f661070

--- php/SDUI/sdui.js
+++ php/SDUI/sdui.js
@@ -6,12 +6,12 @@ sdui.toUrl=function(params,action){
 	var s=[];
 	s.push(sdui.base,sdui.askmark);
 	if(!Object.isArray(sdui.baseParams))for(k in sdui.baseParams){
-			v=sdui.baseParams[k];
+			v=encodeURIComponent(sdui.baseParams[k]);
 			s.push(k,sdui.eqnmark,v,sdui.andmark);
 	}
 	s.push(sdui.actionName,sdui.eqnmark,action,sdui.andmark);
 	for(k in params){
-			v=params[k];
+			v=encodeURIComponent(params[k]);
 			s.push(k,sdui.eqnmark,v,sdui.andmark);
 	}
 	var c=s.pop();
