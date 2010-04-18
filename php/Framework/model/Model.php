02115267c0e4c2edf4cc257d03ad2a3f73478f44
liuchuanren <liuchuanren@720cc348-b754-0410-aa88-cb743f661070>
2010-04-18 19:20:34 +0000

    git-svn-id: https://global.googlecode.com/svn/trunk@177 720cc348-b754-0410-aa88-cb743f661070

--- php/Framework/model/Model.php
+++ php/Framework/model/Model.php
@@ -1,5 +1,7 @@
 <?php
 class Model{
+	public $_record="ModelRecord";
+	public $_store="ModelStore";
 	public $framework=null;
 	public $id="";
 	public $fields=array();
@@ -7,8 +9,8 @@ class Model{
 		return in_array($key,$this->keys);
 	}
 	public function record($data=array(),$id=null){
-		require_once("ModelRecord.php");
-		$name="{$this->id}ModelRecord";
+		require_once("{$this->_record}.php");
+		$name="{$this->id}{$this->_record}";
 		require_once("{$this->framework->path}/model/$name.php");
 		$record=new $name();
 		$record->model=&$this;
@@ -16,8 +18,8 @@ class Model{
 		return $record;
 	}
 	public function store(){
-		require_once("ModelStore.php");
-		$name="{$this->id}ModelStore";
+		require_once("{$this->_store}.php");
+		$name="{$this->id}{$this->_store}";
 		require_once("{$this->framework->path}/model/$name.php");
 		$store=new $name();
 		$store->model=&$this;
---
+++ php/Framework/model/ModelSQLStore.php
@@ -0,0 +1,42 @@
+<?php
+require_once("ModelStore.php");
+class ModelSQLStore extends ModelStore{
+	public $sql;
+	public $table;
+	public $id;
+	public $fields;
+	public function construct(){
+		if(!$this->sql)$this->sql=$this->model->framework->sql;
+		if(!$this->table)$this->table=strtolower($this->model->id);
+		if(!$this->id)$this->id="id";
+		if(!$this->fields)$this->fields=$this->model->fields;
+		$formats=array();
+		$fields=array();
+		foreach($this->model->fields as $index=>$key){
+			$formats[]="'%s'";
+			$fields[]="`{$this->fields[$index]}` as `{$key}`";
+		}
+		$this->_insert="insert into `{$this->table}` (`".implode("`,`",$this->fields)."`)values(".implode(",",$formats).")";
+		$this->_select="select `{$this->id}`,".implode(",",$fields)." from `{$this->table}` where %s like '%s' %s";
+		parent::construct();
+	}
+	public function add($record){
+		$values=array();
+		foreach($this->model->fields as $index=>$key){
+			$values[]=$record->get($key);
+		}
+		$this->sql->query($this->_insert,$values);
+		return $this->sql->affectedRows()==1;
+	}
+	public function query($key,$value,$start=0,$limit=0){
+		if($limit)$limit="limit $start, $limit";
+		else $limit="";
+		$this->sql->query($this->_select,$key,$value,$limit);
+		$records=array();
+		while($row=$this->sql->getRow()){
+			$records[]=$this->model->record($row,$row[$this->id]);
+		}
+		return $records;
+	}
+}
+?>
