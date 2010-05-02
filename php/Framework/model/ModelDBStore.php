622db30f1c27804f067b4990390f29c6c155ea9c
liuchuanren <liuchuanren@720cc348-b754-0410-aa88-cb743f661070>
2010-05-02 07:11:20 +0000

    php/Framework/model/ModelDBStore: support complex join
    git-svn-id: https://global.googlecode.com/svn/trunk@198 720cc348-b754-0410-aa88-cb743f661070

--- php/Framework/model/ModelDBStore.php
+++ php/Framework/model/ModelDBStore.php
@@ -4,10 +4,9 @@ class ModelDBStore extends ModelSQLStore{
 	public $ids;
 	public $tables;
 	public $Fields;
-	public $relation;
-	public $addRelation=true;
-	public $commitRelation=true;
-	public $removeRelation=false;
+	public $autoAdd=true;
+	public $autoCommit=true;
+	public $autoRemove=false;
 	public function construct(){
 		if(!$this->ids)$this->ids=array($this->id);
 		if(!$this->sql)$this->sql=$this->model->framework->sql;
@@ -15,20 +14,28 @@ class ModelDBStore extends ModelSQLStore{
 			if(!$this->table)$this->table=strtolower($this->model->id);
 			$this->tables=array($this->table);
 		}
+		$this->tables[0]=array($this->tables[0]);
+		$this->_join=array();
+		for($i=1,$ii=count($this->$tables);$i<$ii;$i++){
+			$table=&$this->tables[$i];
+			if(is_array($table))$table=array($table);
+			$count=count($table);
+			if($count==1){
+				$table[1]="`{$table[0]}`.`{$this->ids[$i]}`=`{$this->tables[0][0]}`.`{$this->ids[0]}`";
+				$table[2]="inner";
+			}elseif($count==2){
+				$table[2]="inner";
+			}
+			$this->_join[]="{$table[2]} join `{$table[0]}` on {$table[1]}";
+		}
+		$this->_join=implode(" ",$this->_join);
 		if(!$this->Fields){
 			if(!$this->fields)foreach($this->model->fields as $field)$this->fields[]=$field->mapping;
 			$this->Fields=array($this->fields);
 		}
-		if(!$this->relation){
-			$this->relation=array();
-			foreach($this->ids as $index=>$id){
-				$this->relation[]="`{$this->tables[$index]}`.`{$this->ids[$index]}`";
-			}
-			$this->relation=implode("=",$this->relation);
-		}
 		$index=0;
 		foreach($this->Fields as $i=>$fields)foreach($fields as $j=>$mapping){
-			$this->model->fields[$index]->mapping=array($this->tables[$i],$mapping);
+			$this->model->fields[$index]->mapping=array($this->tables[$i][0],$mapping);
 			$index++;
 		}
 		$mappings=array();
@@ -42,14 +49,14 @@ class ModelDBStore extends ModelSQLStore{
 			$fields[]="`{$field->mapping[0]}`.`{$field->mapping[1]}` as `{$field->name}`";
 		}
 		foreach($this->ids as $index=>$id){
-			$table=$this->tables[$index];
+			$table=$this->tables[$index][0];
 			$this->_insert[$table]="insert into `$table` (`".implode("`,`",$mappings[$table])."`)values(".implode(",",$formats[$table]).")";
 			$this->_update[$table]="update `$table` set ".implode(",",$sets[$table])." where `{$this->ids[$index]}`='%s' limit 1";
 			$fields[]="`$table`.`{$this->ids[$index]}` as `_id_$index`";
 		}
 		$this->_select_fields=implode(",",$fields);
-		$this->_select_from="`".implode("`,`",$this->tables)."`";
-		$this->_select_where="{$this->where} and {$this->relation}";
+		$this->_select_from="`{$this->tables[0][0]}` {$this->_join}";
+		$this->_select_where=$this->where;
 		$this->_select="select %s from {$this->_select_from} where {$this->_select_where}";
 		ModelStore::construct();
 	}
@@ -59,7 +66,7 @@ class ModelDBStore extends ModelSQLStore{
 			$values[$field->mapping[0]][]=$record->get($field->name);
 		}
 		foreach($this->Fields as $index=>$fields){
-			$table=$this->tables[$index];
+			$table=$this->tables[$index][0];
 			$this->sql->query($this->_insert[$table],$values[$table]);
 			if($this->sql->affectedRows()==1){
 				$record->data["_id_$index"]=$this->sql->insertId();
@@ -76,19 +83,19 @@ class ModelDBStore extends ModelSQLStore{
 			$values[$field->mapping[0]][]=$record->get($field->name);
 		}
 		foreach($this->Fields as $index=>$fields){
-			$table=$this->tables[$index];
+			$table=$this->tables[$index][0];
 			$this->sql->query($this->_update[$table],$values[$table],$record->data["_id_$index"]);
 			if($this->sql->affectedRows()<0)return false;
 		}
 		return true;
 	}
 	public function remove($record){
-		if($this->removeRelation)foreach($this->Fields as $index=>$fields){
-			$table=$this->tables[$index];
+		if($this->autoRemove)foreach($this->Fields as $index=>$fields){
+			$table=$this->tables[$index][0];
 			$this->sql->query("delete from `$table` where `{$this->ids[$index]}`='%s' limit 1",$record->data["_id_$index"]);
 			if($this->sql->affectedRows()<0)return false;
 		}else{
-			$this->sql->query("delete from `{$this->tables[0]}` where `{$this->ids[0]}`='%s' limit 1",$record->data["_id_0"]);
+			$this->sql->query("delete from `{$this->tables[0][0]}` where `{$this->ids[0]}`='%s' limit 1",$record->data["_id_0"]);
 			return $this->sql->affectedRows()==1;
 		}
 		return true;
